6a07a7939da3fc81ffdb984f6b3bdf8e4b1a4386
amagiddmxh <1107537248@qq.com>
2020-07-22 21:31:25 +0800

    Updates

---
+++ precache-manifest.74631aaf274b153b0084e3a8551c6287.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5311c1a5b5efbf40171caeb48ca7d4f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "206d7e21eaa9e907f8a2",
+    "url": "/static/css/main.523aba22.chunk.css"
+  },
+  {
+    "revision": "ca58ca7001c6f8613262",
+    "url": "/static/js/2.27957215.chunk.js"
+  },
+  {
+    "revision": "8d296bcdc605d24bc0aeff0dd5761cc2",
+    "url": "/static/js/2.27957215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206d7e21eaa9e907f8a2",
+    "url": "/static/js/main.ef581d94.chunk.js"
+  },
+  {
+    "revision": "19bca6c9128f7ce4efd3",
+    "url": "/static/js/runtime-main.9b08dd9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
